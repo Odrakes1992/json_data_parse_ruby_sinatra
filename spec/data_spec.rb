d51fbf4183fd6295fc677fd431a0b589126e563d
Osneil Drakes <osneil-drakes@hotmail.com>
2022-01-31 14:31:54 +0000

    update tests

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -1,18 +1,29 @@
 require 'data'
 
 describe Data do
-  
-  subject(:test) {Data.new('Madrid')}
 
   x = '[{"name": "Homer Simpson", "location": "Springfield", "date_of_birth": "1956-05-12"},{"name": "Frank Reynolds", "location": "Philidelphia","date_of_birth": "1944-11-17"}, {"name": "Diane Nguyen","location": "Los Angeles","date_of_birth": "1980-03-19"},{"name": "Krusty the Clown", "location": "Springfield","date_of_birth": "1957-10-29"}]'
     
+  subject(:test) {Data.new('springfield', x)}
 
   describe "#recognise location" do 
-    it "should return location" do
-      expect(test.location).to eq "Madrid"
+    it "should return location in lowercase" do
+      expect(test.location).to eq "springfield"
     end
   end
 
+  describe "#parsing data" do 
+    it "should return the number of models available based on data" do 
+      test.parse_data
+      expect(test.result.length).to eq 2
+    end
+
+    it "should return available models based on data" do 
+      test.parse_data
+      expect(test.result).to eq ["Homer Simpson", "Krusty the Clown"]
+    end
+
+  end
 
 
 end 
