d815a3bf58fc88ae33ac0411b1a1941fb970cba5
Osneil Drakes <osneil-drakes@hotmail.com>
2022-01-31 12:09:01 +0000

    update routes and save data via params and sessions

--- app.rb
+++ app.rb
@@ -1,8 +1,31 @@
 require 'sinatra'
+require 'sinatra/reloader' 
+require 'sinatra/base'
 
+class DataParser < Sinatra::Base
+  configure :development do 
+    register Sinatra::Reloader
+  end 
+  enable :sessions
 
 
 get '/' do 
   # "Hello world"
   erb(:index)
+end
+
+post '/conversion' do 
+  session[:json_data] = params[:json_data]
+  session[:location] = params[:location]
+  redirect '/result'
+end
+
+get '/result' do
+  @json_data = session[:json_data]
+  @location = session[:location]
+  erb(:result)
+end
+
+run! if app_file == $0
+
 end
